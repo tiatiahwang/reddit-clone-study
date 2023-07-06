2c918e50a0aa229c46a703c7a324253533a92b24
tiatiahwang <tiahwang44@gmail.com>
2023-07-05 19:08:06 -0700

    next-auth google login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import { Icons } from './Icons';
 import { buttonVariants } from './ui/Button';
+import { getAuthSession } from '@/lib/auth';
 
 async function NavBar() {
+  const session = await getAuthSession();
+
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -14,9 +17,16 @@ async function NavBar() {
           </p>
         </Link>
         {/* 검색창 */}
-        <Link href='/sign-in' className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <p>you logged in</p>
+        ) : (
+          <Link
+            href='/sign-in'
+            className={buttonVariants()}
+          >
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -21,7 +21,7 @@ const UserAuthForm: FC<UserAuthFormProps> = ({
   const loginWithGoogle = async () => {
     setIsLoading(true);
     try {
-      throw new Error();
+      await signIn('google');
     } catch (e) {
       // toast notification
       toast({
