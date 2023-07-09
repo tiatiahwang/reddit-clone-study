79e8261fcb300cff9a2433c5a151502366a05e77
tiatiahwang <tiahwang44@gmail.com>
2023-07-09 12:53:17 -0700

    creating subreddit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import NavBar from '@/components/NavBar';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/Toaster';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
@@ -28,12 +29,14 @@ export default function RootLayout({
       )}
     >
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <NavBar />
-        {authModal}
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-          {children}
-        </div>
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <NavBar />
+          {authModal}
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
+            {children}
+          </div>
+        </Providers>
         <Toaster />
       </body>
     </html>
